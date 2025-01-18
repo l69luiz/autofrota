797af3106f7c1baa1f64fe07199adb69774e7e8b
l69luiz <l69luiz@gmail.com>
2025-01-18 11:19:04 -0300

    Feat:BuscaCidades

--- src/pages/cidades/ListagemDeCidade.tsx
+++ src/pages/cidades/ListagemDeCidade.tsx
@@ -2,6 +2,7 @@ import React, { useMemo } from "react";
 import { LayoutBaseDePagina } from "../../shared/layouts/LayoutBaseDePaginas";
 import { FerramentasDaListagem } from "../../shared/components";
 import { useSearchParams } from "react-router-dom";
+
 export const ListagemDeCidade: React.FC =() => {
     
     const [searchParams, setSearchParams] = useSearchParams();
